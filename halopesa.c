699501059c70a1137bbb109c17b49a298e4a0b0a
collins <collinsboniface1000@gmail.com>
2022-01-17 22:54:59 +0200

    Added the input of the phone number in at the startup of the program

--- halopesa.c
+++ halopesa.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include "mainmenu.h"
 
+int phonenumber[10];
 
 int main() {
+
+	int size;
+
+	printf("Enter the size of the number: ");
+	scanf("%d", &size);
+
+	/*
+	 * Bound checking of the array 
+	 */
+	if (size > 10) {
+		printf("Size out of bound");
+	}
+	else {
+
+		if (phonenumber) {
+			printf("Enter phone number: ");
+			for (int i = 0; i < size; i++) {
+				scanf("%d",phonenumber[i]);
+			}
+			printf("\n");
+			printf("This is you phone number: ");
+			for (int i = 0; i < size; i++) {
+				printf("%d", phonenumber[i]);
+			}
+
+			printf("\n \n");
+		}
+	}
+
 	mainmenu();
 }
