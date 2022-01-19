e96f3cdf9cb32c18649c146577655aba7aa02984
collins <collinsboniface1000@gmail.com>
2022-01-19 06:28:20 +0200

    Using struct to my main function and creating an instance of the struct and calling my mainmenu function

--- halopesa.c
+++ halopesa.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "mainmenu.h"
 
-int phonenumber[10];
+
+/*
+ * Using the struct for the user
+ */
+struct user {
+	char phone[50];
+	char password[50];
+	float balance;
+};
 
 int main() {
 
-	int size;
-
-	printf("Enter the size of the number: ");
-	scanf("%d", &size);
-
-	/*
-	 * Bound checking of the array 
-	 */
-	if (size > 10) {
-		printf("Size out of bound");
-	}
-	else {
-
-		if (phonenumber == phonenumber) {
-			printf("Enter phone number: ");
-			for (int i = 0; i < size; i++) {
-				scanf("%d", &phonenumber[i]);
-			}
-			printf("\n");
-			printf("This is you phone number: ");
-			for (int i = 0; i < size; i++) {
-				printf("%d", phonenumber[i]);
-			}
-
-			printf("\n \n");
-		}
-	}
-
-	mainmenu();
+	struct user mteja;
+	mainmenu(); 
+	return 0;
 }
