d63e6bed2c186f6d91786a3372ab9d61769121e8
collins <collinsboniface1000@gmail.com>
2022-01-19 07:54:05 +0200

    Update the main function of the program to as the user to register or login and creating a file if the user wants to create the account

--- halopesa.c
+++ halopesa.c
@@ -18,7 +18,51 @@ int main() {
 	// The instance of the struct
 	struct user mteja;
 
+	// Defining a file pointer
+	FILE *fp;
 
+	// Defining the file name 
+	char filename[50];
+
+	// Declaring the opt variable to check for the user option
+	int opt;
+
+	printf("\t \t \tWelcome to Halopesa \n");
+	printf("1. Register an account \n");
+	printf("2. Login to an account \n \n");
+
+	printf("Your choice: \t");
+	scanf("%d", &opt);
+	
+	// Checking for the user input 
+	if (opt == 1) {
+		system("clear");
+		printf("Ingiza number ya simu: \t");
+		scanf("%s", mteja.phone);
+		printf("Ingiza number la siri: \t");
+		scanf("%s", mteja.password);
+		
+		// Assigning the balance as 0 to the user account
+		mteja.balance = 0;
+
+		// using the copy function to copy the phone number to be the file name
+		strcpy(filename, mteja.phone);
+
+		/*
+		 * Opening the file and concatenate the phone number with the file extension
+		 * Open the file into write mode
+		 * Using the if else condtion to check for the existence of the file if is null
+		 */
+		fp = fopen(strcat(filename, ".no"), "w");
+		fwrite(&mteja, sizeof(struct user), 1, fp);
+		if (fwrite != 0) {
+			printf("\n \n Account succesfully registered");
+		} else {
+			printf("\n \n Something went wrong please try again");
+		}
+	}
+
+	
 	mainmenu(); 
 	return 0;
 }
