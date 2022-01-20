42bf470ec4816096a96a39257dbb0cdf7cac7fa9
collins <collinsboniface1000@gmail.com>
2022-01-20 21:08:08 +0200

    Update to the tumapesa function by adding other option and creating other function for the menu so that they can be called

--- lib/tumapesa.c
+++ lib/tumapesa.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 
+
+// Function prototype
+void kwendaHalopesa();
+void kwendaMitandaoMingine();
+void kamaVocha();
+void nyuma();
+
+
 void tumaPesa() {
+
+	int userInput;
+
 	printf("1. Kwenda Halopesa \n");
 	printf("2. Kwenda Mitandao Mingine \n");
-	printf("3.Kama Vocha \n");
+	printf("3. Kama Vocha \n");
 	printf("# Nyuma \n");
+
+	scanf("%d", &userInput);
+
+	switch (userInput) {
+		case 1:
+			kwendaHalopesa();
+			break;
+		case 2:
+			kwendaMitandaoMingine();
+			break;
+		case 3:
+			kamaVocha();
+			break;
+		case #:
+			nyuma();
+			break;
+	}
+
+}
+
+void kwendaHalopesa() {
+	printf("Kwenda Halopesa");
+}
+
+void kwendaMitandaoMingine() {
+	printf("Kwenda  Mitandao Mingine");
+}
+
+void kamaVocha() {
+	printf("Kama Vocha");
+}
+
+void nyuma() {
+	printf("Nyuma");
 }
